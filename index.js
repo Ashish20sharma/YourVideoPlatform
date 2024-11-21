298acd86629b037d39221b667b461505ce3d1ca4
Ashish20sharma <133103644+Ashish20sharma@users.noreply.github.com>
2024-11-21 23:56:41 +0530

    Listen Server

---
+++ index.js
@@ -0,0 +1,23 @@
+require("module-alias/register");
+
+//require modules
+const http=require("http");
+const express=require("express");
+const path=require("path");
+const session=require("express-session");
+const socketio=require("socket.io");
+
+const app=express();
+const server=http.createServer(app);
+//Attack socket.io to the HTTP server
+const io=socketio(server);
+
+module.exports={io,app};
+
+io.on("connection",(socket)=>{
+    console.info("A Clint Connected.")
+})
+
+server.listen(4000,()=>{
+    console.log("server listening port 4000");
+})
